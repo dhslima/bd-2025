ee8204454e700dcb480cfc6f4c3ee5ce629a987b
David Lima <david.lima@ifal.edu.br>
2025-08-21 10:34:18 -0300

    Respostas das atividades
    Aulas dos dias 15 e 16 de agosto

--- bimestre-3/aulas/escola-query.sql
+++ bimestre-3/aulas/escola-query.sql
@@ -1,11 +1,19 @@
 -- Liste os nomes dos alunos da cidade Recife.
+SELECT nome FROM alunos WHERE cidade = 'Recife';
 
 -- Quais cursos são da área Dados? Mostre titulo e carga_horas.
+SELECT titulo, carga_horas FROM cursos WHERE area = 'Dados';
 
 -- Nomes dos alunos matriculados em qualquer turma do curso "SQL para Iniciantes".
+SELECT nome FROM alunos WHERE id IN (
+SELECT FK_aluno_id FROM matriculas WHERE FK_turma_id IN (
+SELECT id FROM turmas WHERE FK_curso_id = (
+SELECT id FROM cursos WHERE titulo = 'SQL para Iniciantes'))) ORDER BY nome;
 
 -- Alunos com média >= 8.0 em pelo menos uma de suas matrículas.
-
+SELECT nome FROM alunos WHERE id IN (
+SELECT FK_aluno_id FROM matriculas WHERE id IN (
+SELECT DISTINCT (FK_matricula_id) FROM avaliacoes WHERE nota >= 8)) ORDER BY nome;
 -- Turmas com pelo menos 5 matrículas. Mostre id.
 
 -- O curso cuja carga_horas é maior do que a média de todas as cargas.
--- bimestre-3/aulas/loja-atividade-1508.sql
+++ bimestre-3/aulas/loja-atividade-1508.sql
@@ -1,5 +1,16 @@
 -- Quais vendedores venderam o Produto 1?
+SELECT nome FROM vendedor WHERE cod_vendedor IN (
+SELECT DISTINCT(FK_cod_vendedor) FROM pedido WHERE num_pedido IN (
+SELECT FK_num_pedido FROM itempedido WHERE FK_cod_produto = 1));
 
 -- Quais produtos foram comprados pelo Cliente 1?
+SELECT nome FROM produto WHERE cod_produto IN (
+SELECT DISTINCT (FK_cod_produto) FROM itempedido WHERE FK_num_pedido IN (
+SELECT num_pedido FROM pedido WHERE FK_cod_cliente = 1));
 
 -- Quais clientes compraram o produto mais caro do catálogo?
+SELECT nome FROM cliente WHERE cod_cliente IN (
+SELECT FK_cod_cliente FROM pedido WHERE num_pedido IN (
+SELECT FK_num_pedido FROM itempedido WHERE FK_cod_produto IN (
+SELECT cod_produto FROM produto WHERE valor_unitario = (
+SELECT MAX(valor_unitario) FROM produto)))) ORDER BY nome;
