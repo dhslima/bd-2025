4228372d1a2e0b9cabbf91923a39e6d61434e75e
David Lima <david.lima@ifal.edu.br>
2025-10-30 10:09:22 -0300

    atualizando

--- bimestre-4/nodejs/queries.js
+++ bimestre-4/nodejs/queries.js
@@ -1,5 +1,7 @@
 import { pool } from "./db.js";
 
+// node queries.js
+
 async function selectBasico() {
   console.log("1) SELECT * FROM alunos LIMIT 5;");
   const r = await pool.query(`SELECT * FROM alunos ORDER BY id LIMIT 5;`);
@@ -31,10 +33,27 @@ async function join1N_TurmasCurso() {
 
 async function joinNM_MatriculasDetalhe() {
   console.log("\n4) JOIN N-M: matriculas com aluno, turma e curso");
+  const r = await pool.query(`
+    SELECT a.nome AS aluno, c.titulo AS curso, c.area AS area, t.ano AS ano, t.semestre AS semestre FROM alunos a 
+    INNER JOIN matriculas m ON a.id = m.fk_aluno_id
+    INNER JOIN turmas t ON m.fk_turma_id = t.id
+    INNER JOIN cursos c ON t.fk_curso_id = c.id
+    ORDER BY a.nome
+    `);
+  console.table(r.rows);
 }
 
 async function notasPorMatricula() {
   console.log("\n5) Notas por matrícula (avaliacoes)");
+  const r = await pool.query(
+    `SELECT a.nome AS aluno, av.nota AS nota, av.tipo AS tipo , c.titulo AS curso
+    FROM avaliacoes av INNER JOIN matriculas m ON av.fk_matricula_id = m.id 
+    INNER JOIN alunos a ON m.fk_aluno_id = a.id 
+    INNER JOIN turmas t ON m.fk_turma_id = t.id
+    INNER JOIN cursos c ON t.fk_curso_id = c.id
+    ORDER BY a.nome, c.titulo, av.tipo;`
+  );
+  console.table(r.rows);
 }
 
 async function mediaPorAluno() {
